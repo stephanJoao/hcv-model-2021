f4476a360a949786e6617124cde29ebafcf895b2
João Stephan <joaossmauricio@gmail.com>
2021-12-03 16:55:44 -0300

    Organizada e limpeza geral no repositório

--- src/HCV_Model.h
+++ src/HCV_Model.h
@@ -14,13 +14,13 @@
 using namespace std;
 
 // const int    TIME  = 50; //tempo de simulacao
-const int    AGE   = 500; // idade da infecao (dias)
-const int    buffer   = 2;
+const int AGE = 500; // idade da infecao (dias)
+const int buffer = 2;
 
-class HCV_Model{
+class HCV_Model
+{
 
-  private:
-
+private:
     double I[buffer][AGE]; //celulas infectadas
 
     double Rp[buffer][AGE]; // RNA intracelular positivo
@@ -40,50 +40,49 @@ class HCV_Model{
     int vardelta;
     int varrho;
 
-    double  N; //valores iniciais
+    double N; //valores iniciais
 
-
-    double s; // taxa constante de producao de celulas alvo
-    double d; // decaimento natural de celulas alvo
-    double beta; // taxa de infecao
+    double s;     // taxa constante de producao de celulas alvo
+    double d;     // decaimento natural de celulas alvo
+    double beta;  // taxa de infecao
     double delta; // decaimento de celulas infectadas
-    double rho; // taxa de exporta��o de RNA positivo
-    double c; // taxa de eliminacaoo do virus pelo SI
+    double rho;   // taxa de exporta��o de RNA positivo
+    double c;     // taxa de eliminacaoo do virus pelo SI
 
     double alpha; // taxa de replica��o de RNA positivo
 
     double r; // taxa de replica��o de RNA negativo / complexo de replica��o (Rn)
 
-    double k; // coeficiente da fun��o exponencial de atraso na exporta��o de RNA positivo
-    double tau; // tempo de atraso para a exporta��o de RNA positivo
-    double n; //atraso de delta
-    double Rmax; // n�mero m�ximo de RNA negativo / complexo de replica��o (Rn)
+    double k;     // coeficiente da fun��o exponencial de atraso na exporta��o de RNA positivo
+    double tau;   // tempo de atraso para a exporta��o de RNA positivo
+    double n;     //atraso de delta
+    double Rmax;  // n�mero m�ximo de RNA negativo / complexo de replica��o (Rn)
     double sigma; // taxa de forma��o de complexos de replica��o
-    double mu_t; // decaimento natural de Rt
+    double mu_t;  // decaimento natural de Rt
     double theta; // taxa de disponibilidade para tradu��o
-    double mu_c; // decaimento natural de Rc e Rn
+    double mu_c;  // decaimento natural de Rc e Rn
 
-    double epsilon_s; // efetividade da terapia em diminuir ou bloquear a exporta��o de RNA positivo
+    double epsilon_s;     // efetividade da terapia em diminuir ou bloquear a exporta��o de RNA positivo
     double epsilon_alpha; // efetividade da terapia em diminuir ou bloquear a replica��o de RNA positivo
-    double epsilon_r; // efetividade da terapia em diminuir ou bloquear a replica��o de RNA negativo
+    double epsilon_r;     // efetividade da terapia em diminuir ou bloquear a replica��o de RNA negativo
 
     double kappa_t; // fator para aumentar a degrada��o de RNA positivo dispon�vel para tradu��o
     double kappa_c; // fator para aumentar a degrada��o de RNA positivo e negativo no complexo de replica��o
 
     int saveFiles;
     char *dir;
-    FILE* dataInfected;
-    FILE* dataVirus;
-    FILE* dataTarget;
-    FILE* dataRNA_Positivo;
-    FILE* dataRNA_Negativo;
-    FILE* dataRNA_Traduzido;
-    FILE* datadelta;
-    FILE* datarho;
+    FILE *dataInfected;
+    FILE *dataVirus;
+    FILE *dataTarget;
+    FILE *dataRNA_Positivo;
+    FILE *dataRNA_Negativo;
+    FILE *dataRNA_Traduzido;
+    FILE *datadelta;
+    FILE *datarho;
 
     std::string Header();
     std::string Footer(long int t);
-    int checkFile(FILE* theFile);
+    int checkFile(FILE *theFile);
     void initialize();
     double calcIntegral(double vec1[][AGE], double vec2[][AGE], double vec3[][AGE]);
     double calcIntegral2(double a, double b, double vec1[][AGE], double vec2[][AGE], double delta, double rho, double deltaA);
@@ -92,19 +91,21 @@ class HCV_Model{
 public:
     HCV_Model();
     int solve();
-
 };
+
 /**
 * Constructor
 */
-HCV_Model::HCV_Model(){
-
+HCV_Model::HCV_Model()
+{
+    
 }
 
 /**
 * Set conditions and parameter values
 */
-void HCV_Model::initialize(){
+void HCV_Model::initialize()
+{
     fstream param;
     param.open("parametros_DE.txt");
     std::string aux_string;
@@ -114,10 +115,10 @@ void HCV_Model::initialize(){
 
     getline(param, aux_string, ',');
     epsilon_r = atof(aux_string.c_str());
-    
+
     getline(param, aux_string, ',');
     epsilon_alpha = atof(aux_string.c_str());
-    
+
     getline(param, aux_string, ',');
     epsilon_s = atof(aux_string.c_str());
 
@@ -126,19 +127,19 @@ void HCV_Model::initialize(){
 
     getline(param, aux_string, ',');
     r = atof(aux_string.c_str());
-        
+
     getline(param, aux_string, ',');
     delta = atof(aux_string.c_str());
-    
+
     getline(param, aux_string, ',');
     mu_c = atof(aux_string.c_str());
 
     getline(param, aux_string, ',');
     rho = atof(aux_string.c_str());
-    
+
     getline(param, aux_string, ',');
     theta = atof(aux_string.c_str());
-    
+
     getline(param, aux_string, ',');
     sigma = atof(aux_string.c_str());
 
@@ -150,11 +151,11 @@ void HCV_Model::initialize(){
     /**
     * number of days simulated
     */
-    days      = 72;
+    days = 72;
     /**
     * number of points saved
     */
-    points    = 10;
+    points = 10;
     /**
     * output directory
     */
@@ -162,71 +163,77 @@ void HCV_Model::initialize(){
     /**
     * simulation
     */
-    deltaT     = 0.01; //pow(10,-4);
+    deltaT = 0.01; //pow(10,-4);
     /**
     * age
     */
-    deltaA     = 0.1;
+    deltaA = 0.1;
     /**
     * number of iterations per day
     */
     iterPerDay = 100;
 
-    tol        = pow(10,-4);
+    tol = pow(10, -4);
 
     /**
      * Vary delta 1, fix delta 0
      */
-    vardelta = 0;//1; // 0;
+    vardelta = 0; //1; // 0;
     /**
      * Vary rho 1, fix delta 0
      */
-    varrho = 1;//1; // 0;
+    varrho = 1; //1; // 0;
 
     /**
     * baseline parameters
     */
-    d     = 0.010;
-    s     = 130000;
+    d = 0.010;
+    s = 130000;
     // delta = 0.58;
-    beta  = 5*pow(10,-8);
+    beta = 5 * pow(10, -8);
     // c     = 22.30;
     // rho   = 8.180;
     // alpha = 30.0;
-    Rmax  = 50.0;
-    // r     = 1.49; 
-    tau   = 0.50;
-    n     = 1.00;
-    k     = 0.80;
-    mu_t  = 0.89; 
-    // mu_c  = 2.55; 
+    Rmax = 50.0;
+    // r     = 1.49;
+    tau = 0.50;
+    n = 1.00;
+    k = 0.80;
+    mu_t = 0.89;
+    // mu_c  = 2.55;
     // sigma = 1.30;
     // theta = 1.20;
     /**
     * therapy parameters
     */
     // epsilon_alpha = 0.928;
-    // epsilon_r     = 0.47; 
+    // epsilon_r     = 0.47;
     // epsilon_s     = 0.998;
-    kappa_t       = 1.00;
-    kappa_c       = 1.00;
-    
-    if ((sigma + rho + mu_c - (sigma*theta)/(theta + rho + mu_t) <=0) || (alpha*r -
-     (sigma + rho + mu_c - (sigma*theta)/(theta + rho + mu_t))*mu_c<=0)){
+    kappa_t = 1.00;
+    kappa_c = 1.00;
+
+    if ((sigma + rho + mu_c - (sigma * theta) / (theta + rho + mu_t) <= 0) || (alpha * r -
+                                                                                   (sigma + rho + mu_c - (sigma * theta) / (theta + rho + mu_t)) * mu_c <=
+                                                                               0))
+    {
         sigma = 1.30;
         theta = 1.20;
         epsilon_alpha = 0.928;
-        epsilon_r =0.47;
+        epsilon_r = 0.47;
         // cout << "caiu nas condiçoes de estabilidade!!!!" << endl;
-    }else{
+    }
+    else
+    {
         // cout << "Não entrou nas condiçoes de estabilidade!!!!" << endl;
     }
-    
+
     /**
     * Initial Conditions
     */
-    for(int j=0; j<2;j++){
-        for(int i=0; i<AGE;i++){
+    for (int j = 0; j < 2; j++)
+    {
+        for (int i = 0; i < AGE; i++)
+        {
             I[j][i] = 0.0;
             Rt[j][i] = 0.0;
             Rp[j][i] = 0.0;
@@ -235,73 +242,97 @@ void HCV_Model::initialize(){
     }
     Rt[0][0] = 1.00;
 
-
     double rho1;
-    if (varrho) rho1 = 0.00;// = rho;
-    else rho1 = rho;
+    if (varrho)
+        rho1 = 0.00; // = rho;
+    else
+        rho1 = rho;
 
-    double soma=0;
-    soma+=Rp[0][0] + Rt[0][0];
+    double soma = 0;
+    soma += Rp[0][0] + Rt[0][0];
 
-    for(int a=1; a<AGE;a++){
-        if(varrho){
-            if((deltaA*(double) a)<tau){
-                rho1=0.0;
-            }else{
-                rho1=(1-exp(-k*((deltaA*(double)a)-tau)))*rho;
+    for (int a = 1; a < AGE; a++)
+    {
+        if (varrho)
+        {
+            if ((deltaA * (double)a) < tau)
+            {
+                rho1 = 0.0;
             }
-        }else rho1 = rho;
-        Rn[0][a] = (r*Rp[0][a-1] - r*Rp[0][a-1]*(Rn[0][a-1]/Rmax) -
-                mu_c*Rn[0][a-1])*deltaA + Rn[0][a-1];
+            else
+            {
+                rho1 = (1 - exp(-k * ((deltaA * (double)a) - tau))) * rho;
+            }
+        }
+        else
+            rho1 = rho;
+        Rn[0][a] = (r * Rp[0][a - 1] - r * Rp[0][a - 1] * (Rn[0][a - 1] / Rmax) -
+                    mu_c * Rn[0][a - 1]) *
+                       deltaA +
+                   Rn[0][a - 1];
 
-        Rp[0][a] = (alpha*Rn[0][a-1] + sigma*Rt[0][a-1] -
-                theta*Rp[0][a-1]- rho1*Rp[0][a-1] -
-                mu_c*Rp[0][a-1])*deltaA + Rp[0][a-1];
+        Rp[0][a] = (alpha * Rn[0][a - 1] + sigma * Rt[0][a - 1] -
+                    theta * Rp[0][a - 1] - rho1 * Rp[0][a - 1] -
+                    mu_c * Rp[0][a - 1]) *
+                       deltaA +
+                   Rp[0][a - 1];
 
-        Rt[0][a] = (theta*Rp[0][a-1] - sigma*Rt[0][a-1] -
-                rho1*Rt[0][a-1] - mu_t*Rt[0][a-1])*deltaA + Rt[0][a-1];
+        Rt[0][a] = (theta * Rp[0][a - 1] - sigma * Rt[0][a - 1] -
+                    rho1 * Rt[0][a - 1] - mu_t * Rt[0][a - 1]) *
+                       deltaA +
+                   Rt[0][a - 1];
 
         //if (a<250) soma+=(Rp[0][a] + Rt[0][a]);
 
         //printf("a = %d Rt = %.4lf Rp = %.4lf Rn = %.4lf \n", a, Rt[0][a], Rp[0][a], Rn[0][a]);
     }
-    
-    N = calcIntegral2(0.0, AGE, Rp, Rt, delta, rho, deltaA); 
-    T = c / (beta * N);                                       
+
+    N = calcIntegral2(0.0, AGE, Rp, Rt, delta, rho, deltaA);
+    T = c / (beta * N);
     V = V0; //fixar baseado na outra DE
     I[0][0] = beta * T * V;
 
     double delta1;
-    if (vardelta) delta1 = 0.01;
-    else delta1 = delta;
-    for(int a=1; a<AGE;a++){
+    if (vardelta)
+        delta1 = 0.01;
+    else
+        delta1 = delta;
+    for (int a = 1; a < AGE; a++)
+    {
         //delta variavel
-        if(vardelta) delta1=(1-exp(-k*(a*deltaA)))*delta;
-        else delta1 = delta;
-        I[0][a] = (beta*V*T*exp(-delta1*deltaA*(double)a))*deltaA + I[0][a-1];
-       // printf("a = %d I0 = %lf \n", a, I[0][a]);
+        if (vardelta)
+            delta1 = (1 - exp(-k * (a * deltaA))) * delta;
+        else
+            delta1 = delta;
+        I[0][a] = (beta * V * T * exp(-delta1 * deltaA * (double)a)) * deltaA + I[0][a - 1];
+        // printf("a = %d I0 = %lf \n", a, I[0][a]);
     }
 }
 
 /**
  * Updates the current results to position 1
  */
-void  HCV_Model::update(double vec[][AGE]){
-    for(int a = 0; a < AGE; a++) {
+void HCV_Model::update(double vec[][AGE])
+{
+    for (int a = 0; a < AGE; a++)
+    {
         vec[0][a] = vec[1][a];
-	}
-}
-
-double  HCV_Model::calcIntegral(double vec1[][AGE], double vec2[][AGE],double vec3[][AGE]){
-    int a;
-    double sum=0.0;
-    for(a = 0; a < AGE; a++){
-        sum += (vec1[0][a]+vec2[0][a])*vec3[0][a];
     }
-    return sum/(2.0*AGE);
 }
 
-double  HCV_Model::calcIntegral2(double a, double b, double vec1[][AGE], double vec2[][AGE], double delta, double rho, double deltaA){
+double HCV_Model::calcIntegral(double vec1[][AGE], double vec2[][AGE], double vec3[][AGE])
+{
+    int a;
+    double sum = 0.0;
+    for (a = 0; a < AGE; a++)
+    {
+        sum += (vec1[0][a] + vec2[0][a]) * vec3[0][a];
+    }
+    return sum / (2.0 * AGE);
+}
+
+double HCV_Model::calcIntegral2(double a, double b, double vec1[][AGE], double vec2[][AGE], double delta, double rho, double deltaA)
+{
     Rosetta::GaussLegendreQuadrature<5> gl5;
     //std::cout << std::setprecision(6);
     std::setprecision(6);
@@ -312,7 +343,8 @@ double  HCV_Model::calcIntegral2(double a, double b, double vec1[][AGE], double 
 /******************************************************************************
 * Solve model equations
 *******************************************************************************/
-int HCV_Model::solve(){
+int HCV_Model::solve()
+{
 
     long int t = 0;
     long int a = 0;
@@ -326,88 +358,105 @@ int HCV_Model::solve(){
     /**
     * begin time loop
     */
-    do{
+    do
+    {
 
         // if (t == 0) cout << "Calculating...\n";
 
-        int value = ((int)iterPerDay*days)/points;
-        float time_save = (float) t/(float)iterPerDay;
-        if(V <0){
-            V = -1*V;
-        }
-        if(true) {
+        int value = ((int)iterPerDay * days) / points;
+        float time_save = (float)t / (float)iterPerDay;
+        // if (V < 0)
+        // {
+        //     V = -1 * V;
+        // }
+        if (true)
+        {
             // cout << "Saving files : iteration ..."<< time_save << "\n";
-            
-            saida << time_save << "," << V << endl;
 
+            saida << time_save << "," << V << endl;
         }
         /**
         * ODEs
         */
-        T = (s - d*T - beta*V*T)*deltaT + T;
-        V = ((1-epsilon_s)*rho*calcIntegral(Rp,Rt,I) - c*V)*deltaT + V;
-        
+        T = (s - d * T - beta * V * T) * deltaT + T;
+        V = ((1 - epsilon_s) * rho * calcIntegral(Rp, Rt, I) - c * V) * deltaT + V;
+
         //printf("t = %ld V = %lf T = %lf \n", t,V,T);
         /**
         * begin age loop
         */
-        double rho1;// = rho;
-        double delta1;// = delta;
-        if(varrho)
-            rho1=0.0;
+        double rho1;   // = rho;
+        double delta1; // = delta;
+        if (varrho)
+            rho1 = 0.0;
         else
-            rho1=rho;
-        a=0;
-        Rp[1][a]  = 0.0;
-        Rn[1][a]  = 0.0;
-        Rt[1][a]  = 1.0;
-        I[1][a]   = beta*V*T;
+            rho1 = rho;
+        a = 0;
+        Rp[1][a] = 0.0;
+        Rn[1][a] = 0.0;
+        Rt[1][a] = 1.0;
+        I[1][a] = beta * V * T;
 
-        if (vardelta) delta1=0.01;
-        else delta1 = delta;
+        if (vardelta)
+            delta1 = 0.01;
+        else
+            delta1 = delta;
 
-        for(a = 1; a < AGE-1; a++){
-            if(varrho){
-                if(((double)a*deltaA)<tau){
-                    rho1=0;
-                }else{
-                    rho1=(1-exp(-k*(((double)a*deltaA)-tau)))*rho;
+        for (a = 1; a < AGE - 1; a++)
+        {
+            if (varrho)
+            {
+                if (((double)a * deltaA) < tau)
+                {
+                    rho1 = 0;
                 }
-            }else{
+                else
+                {
+                    rho1 = (1 - exp(-k * (((double)a * deltaA) - tau))) * rho;
+                }
+            }
+            else
+            {
                 rho1 = rho;
             }
             //delta variavel
-            if(vardelta)
-                delta1=delta*(1-exp(-k*(a*deltaA)));
+            if (vardelta)
+                delta1 = delta * (1 - exp(-k * (a * deltaA)));
             else
                 delta1 = delta;
 
             // delta variavel
-            I[1][a] = (-delta1*I[0][a]-(I[0][a]-I[0][a-1])/(deltaA))*deltaT + I[0][a];
+            I[1][a] = (-delta1 * I[0][a] - (I[0][a] - I[0][a - 1]) / (deltaA)) * deltaT + I[0][a];
 
-		    Rn[1][a] = ((1-epsilon_r)*r*Rp[0][a] - (1-epsilon_r)*r*Rp[0][a]*(Rn[0][a]/Rmax) -
-                        kappa_c*mu_c*Rn[0][a] - (Rn[0][a]-Rn[0][a-1])/(deltaA))*deltaT + Rn[0][a];
+            Rn[1][a] = ((1 - epsilon_r) * r * Rp[0][a] - (1 - epsilon_r) * r * Rp[0][a] * (Rn[0][a] / Rmax) -
+                        kappa_c * mu_c * Rn[0][a] - (Rn[0][a] - Rn[0][a - 1]) / (deltaA)) *
+                           deltaT +
+                       Rn[0][a];
 
-		    Rp[1][a] = ((1-epsilon_alpha)*alpha*Rn[0][a]+sigma*Rt[0][a]-theta*Rp[0][a] -
-                        (1-epsilon_s)*rho1*Rp[0][a]-kappa_c*mu_c*Rp[0][a] -
-                        (Rp[0][a]-Rp[0][a-1])/(deltaA))*deltaT + Rp[0][a];
+            Rp[1][a] = ((1 - epsilon_alpha) * alpha * Rn[0][a] + sigma * Rt[0][a] - theta * Rp[0][a] -
+                        (1 - epsilon_s) * rho1 * Rp[0][a] - kappa_c * mu_c * Rp[0][a] -
+                        (Rp[0][a] - Rp[0][a - 1]) / (deltaA)) *
+                           deltaT +
+                       Rp[0][a];
 
-            Rt[1][a] = (theta*Rp[0][a]-sigma*Rt[0][a]-(1-epsilon_s)*rho1*Rt[0][a] -
-                        kappa_t*mu_t*Rt[0][a] - ((Rt[0][a]-Rt[0][a-1])/(deltaA)))*deltaT + Rt[0][a];
-	    }
+            Rt[1][a] = (theta * Rp[0][a] - sigma * Rt[0][a] - (1 - epsilon_s) * rho1 * Rt[0][a] -
+                        kappa_t * mu_t * Rt[0][a] - ((Rt[0][a] - Rt[0][a - 1]) / (deltaA))) *
+                           deltaT +
+                       Rt[0][a];
+        }
 
-	    update(I);
-	    update(Rn);
-	    update(Rp);
-	    update(Rt);
+        update(I);
+        update(Rn);
+        update(Rp);
+        update(Rt);
 
         t++;
 
-    }while (t < (iterPerDay*days)) ;
+    } while (t < (iterPerDay * days));
 
     saida.close();
 
     return 0;
- }
+}
 
 #endif
