7b4e0b8358ea8894268f14682df86c24cb747725
Joao Stephan <joaossmauricio@gmail.com>
2022-02-23 14:54:09 -0300

    Corrigi o plot dade dados, e removi o V0 dos parametros, já que ele é obtido com os dados experimentais

--- differential_evolution/plotData.py
+++ differential_evolution/plotData.py
@@ -5,42 +5,68 @@ import matplotlib.pyplot as plt
 
 selected_patient = "PATB06"
 
+
+def patients_names(dir):
+      """
+      Gets patients names in a dataframe.
+      Arguments:
+            dir: directory and name of .csv that has the experimental data
+      Returns:
+            l: list of patients names
+      """
+      df = pd.read_csv(dir)
+      
+      l = []
+      for i in range(len(df.columns) // 2):
+            l.append(df.columns[i*2 + 1])
+      
+      return l
+
+
 # Directory of experimental data
-exp_data = "differential_evolution/data/experimentalData.csv"
+dir_exp_data = "differential_evolution/data/experimentalData.csv"
 
 # Directory of diferential evolution parameters
-DE_params = "differential_evolution/output/DE_parameters.csv"
+dir_de_params = "differential_evolution/output/DE_parameters.csv"
 
 # Reads experimental data
-df_exp_data = pd.read_csv(exp_data)
+df_exp_data = pd.read_csv(dir_exp_data)
 
-# Gets patient's names
+# Gets patients' names
 patients = []
 for i in range(len(df_exp_data.columns) // 2):
       patients.append(df_exp_data.columns[i*2 + 1])
 
+# Reads experimental data of selected patient
+df = df_exp_data[selected_patient]
+exp_data = df.to_numpy() # already in logaritmic scale
+df = df_exp_data[selected_patient + "_time"]
+exp_data_time = df.to_numpy()
+
+# Prints experimental data of selected patient
 print("Experimental data on " + selected_patient + ": ")
-print(df_exp_data[selected_patient].tolist())
+print(exp_data)
 print("\n")
 
 # Reads diferential evolution parameters
-df_DE_params = pd.read_csv(DE_params)
+df_de_params = pd.read_csv(dir_de_params)
 
 # Gets selected patient parameters
-params = df_DE_params.loc[df_DE_params["Patient"] == selected_patient]
-params = params.values.tolist()
-params = params[0]
+selected_patient_index = df_de_params.index[df_de_params["Patient"] == selected_patient]
+de_params = df_de_params.to_numpy()
+de_params = de_params[selected_patient_index].flatten()
+de_params = np.delete(de_params, 0)
 
+# Prints diferential evolution parameters of selected patient
 print("Parameters of " + selected_patient + ": ")
-print(params)
+print(de_params)
 print("\n")
 
 # Writes parameters on file for model solver
-pat_aux, V0, epsilon_r, epsilon_alpha, epsilon_s, alpha, r, delta, mu_c, rho, theta, sigma, c = params
-
+epsilon_r, epsilon_alpha, epsilon_s, alpha, r, delta, mu_c, rho, theta, sigma, c = de_params
 
 with open("hcv_model/input/parametros_DE.txt", 'w') as params_file:
-      params_file.write(str(V0) + "," + str(epsilon_r) + "," + str(epsilon_alpha) + "," + str(epsilon_s) + "," + str(alpha) + "," + str(r) + "," + str(delta) +  "," + str(mu_c) + "," + str(rho) + "," + str(theta) + "," + str(sigma) + "," + str(c))
+      params_file.write(str(10**exp_data[0]) + "," + str(epsilon_r) + "," + str(epsilon_alpha) + "," + str(epsilon_s) + "," + str(alpha) + "," + str(r) + "," + str(delta) +  "," + str(mu_c) + "," + str(rho) + "," + str(theta) + "," + str(sigma) + "," + str(c))
 
 # Execute the model with the parameters
 cwd = os.getcwd()
@@ -57,15 +83,10 @@ with open(cwd + "/hcv_model/output/saida.txt", 'r') as solved_file:
     for i in data_solved:
         time = np.append(time, float(i[0]))
         viral_load = np.append(viral_load, float(i[1]))
-
 viral_load = np.log10(viral_load)
 
-# Gets data of selected patient
-y = df_exp_data[selected_patient].tolist()
-x = df_exp_data[selected_patient + "_time"].tolist()
-
 # Plots data
-plt.plot(x, y, 'or', label="experimental data")
+plt.plot(exp_data_time, exp_data, 'or', label="experimental data")
 plt.plot(time, viral_load, '-b', label="results model C++")
 plt.title(selected_patient)
 plt.ylabel("Viral load $log_{10}$")
