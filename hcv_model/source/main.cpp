da3f9bd68790cda72dd07c122e4fb879c748deaa
Joao Stephan <joaossmauricio@gmail.com>
2022-03-22 21:15:42 -0300

    Flag de otimização e correção na main - reduzi a execução de um paciente em 40 minutos

--- hcv_model/source/main.cpp
+++ hcv_model/source/main.cpp
@@ -8,14 +8,17 @@ using namespace std;
 int main()
 {  	
 	char cwd[1024];	
-  	getcwd(cwd, sizeof(cwd));
-  	
-	string dir(cwd);
-	string input = dir + "/input/DE_parameters.txt";
-	string output = dir + "/output/solution.txt";
+	
+	if(getcwd(cwd, sizeof(cwd)) != NULL ) 
+	{
+		string dir(cwd);
+		string input = dir + "/input/DE_parameters.txt";
+		string output = dir + "/output/solution.txt";
 
-	HCV_Model* model = new HCV_Model(input, output);
+		HCV_Model* model = new HCV_Model(input, output);
+		model->solve();
+		delete model;
+	}  	
   	
-	model->solve();
 	return 0;
 }
