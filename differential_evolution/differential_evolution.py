c39d48ac0d073b6685fee844a4d33fb45b23c83f
Joao Stephan <joaossmauricio@gmail.com>
2022-04-07 16:46:54 -0300

    Teste pronto para todos os pacientes

--- differential_evolution/differential_evolution.py
+++ differential_evolution/differential_evolution.py
@@ -25,6 +25,7 @@ recombination = 0.5
 # Max number of generations (iterations)
 max_iter = 30
 
+
 if __name__ == "__main__":
 
     # Creation of necessary directory
@@ -41,38 +42,45 @@ if __name__ == "__main__":
 
     # Header of experiment report
     output = open(dir_output + "report_DE.txt", "a")
-    output.writelines("\n\n =========== Experiment ===========\n")
-    output.writelines("\nPopulation size: " + str(pop_size))
-    output.writelines("\nNumber of generations: " + str(max_iter) + "\n")
-    output.writelines("\nParameters: " + array_param + "\n")
+    output.writelines("\n\n=========== Experiment information ===========\n")
+    output.writelines("\nPopulation size -------- " + str(pop_size))
+    output.writelines("\nNumber of generations -- " + str(max_iter) + "\n")
+    output.writelines("\nParameters -- " + array_param)
+    output.writelines("\nBounds ------ " + str(bounds) + "\n\n\n")
+    output.writelines("\n\n")
     output.close()
 
-    # Reads
+    # Reads patients names
     patients = utils.reads_patients_names()
     pat_cont = 1
 
-    for p in patients:
-        output = open(dir_output + "report_DE.txt", "a")
-        output.writelines("- " + p)
+    for i in range(0,len(patients)):
+        
+        if i > 16:
+            output = open(dir_output + "report_DE.txt", "a")
+            output.writelines("- " + patients[i])
 
-        # Gets patient experimental data
-        exp_time, exp_viral_load = utils.reads_experimental_data(p)
+            # Gets patient experimental data
+            exp_time, exp_viral_load = utils.reads_experimental_data(patients[i])
+            
+            # Diferential evolution with time measurement
+            t_ini = time.perf_counter()
+            solution = differential_evolution(cost.model_cost, bounds, args=(exp_time, exp_viral_load), maxiter=max_iter, popsize=pop_size, mutation=mutate, recombination=recombination, tol=0.1, workers=1)
+            t_fin = time.perf_counter()
+            
+            utils.writes_de_parameters(patients[i], solution.x)
+            
+            output.writelines(f"\n  - Execution time: {t_fin - t_ini:0.4f} seconds")
+            # output.writelines('\nCusto do melhor conjunto de parametros: ' + str(solution.fun) + '\n\n')
+            output.writelines("\n  - Solution found: " + str(solution.x) + "\n\n\n")
+            output.close()
+            
+            utils.plot_experiment_patient(patients[i], solution.x)
+            
+            pat_cont += 1    
         
-        # Diferential evolution with time 
-        t_ini = time.perf_counter()
-        solution = differential_evolution(cost.model_cost, bounds, args=(exp_time, exp_viral_load), maxiter=max_iter, popsize=pop_size, mutation=mutate, recombination=recombination, tol=0.1, workers=1)
-        t_fin = time.perf_counter()
-        
-        output.writelines(f"Execution time: {t_fin - t_ini:0.4f} seconds")
-        # output.writelines('\nCusto do melhor conjunto de parametros: ' + str(solution.fun) + '\n\n')
-        output.writelines("\nSolution found: " + str(solution.x) + "\n\n")
 
-        utils.plot_experiment_patient(p, solution.x)
-        
-        pat_cont += 1
-        break
-    
-        #TODO Melhorar o print do report e fazer leitura de parametros - armazenamento - e salvamento/substituição pelo do experimento atual
-        
-
+    # Footer of experiment report
+    output = open(dir_output + "report_DE.txt", "a")
+    output.writelines("\n\n==============================================\n")
     output.close()
